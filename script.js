8deb83aedf4955294b59ab8e65f1896e5a1ee9de
najmamuhudin <najmahmohaaa@gmail.com>
2021-04-28 15:38:57 +0300

    add homepage

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ function pizza(size, crust, toppings){
    this.crust = crust;
    this.toppings = toppings;
 }
+const calculateTotal = document.getElementById("calculate-total");
 
-function total(){
+let anotherOrderPrompt;
+
+function total(e){
+   e.preventDefault();
        var nSize = document.getElementById("size").value;
-       var nCrust = document.getElementById("crust").value;
-       var ntoppings = document.getElementById("toppings").value;
+        var nCrust = document.getElementById("crust").value;
+       var nToppings = document.getElementById("toppings").value;
        var number = document.getElementById("numero").value;
        var deliver = document.getElementById("deliver").checked;
        var location;
@@ -15,49 +19,19 @@ function total(){
        if (deliver === true){
          location = prompt("Where do you stay?");
        }
-
-      var price;
-      var tprice;
-      
-      if(nSize =="large"){
-         price = 1000;
-         if(ntoppings == "extra cheese"){
-            price += 100;
-            tprice = price * number;
-         } else if(ntoppings == "pepperoni"){
-            price += 50;
-            tprice = price* number;
-         } else {
-            price += 150;
-            tprice = price*number;
-         }
-         
-       } else if (nSize == "medium"){
-          price = 800;
-          if(ntoppings == "extra cheese"){
-            price += 100;
-            tprice = price * number;
-         } else if(ntoppings == "pepperoni"){
-            price += 50;
-            tprice = price* number;
-         } else {
-            price += 150;
-            tprice = price*number;
-         }
-
-       } else{
-          price = 500;
-          if(ntoppings == "extra cheese"){
-            price += 100;
-            tprice = price * number;
-         } else if(ntoppings == "pepperoni"){
-            price += 50;
-            tprice = price* number;
-         } else {
-            price += 150;
-            tprice = price*number;
-         }
+       
+       let price;
+       const numberOfOrders = number === '' ? 1 : number;
+       price = (parseInt(nSize)+ parseInt(nCrust) + parseInt(nToppings)) * parseInt(numberOfOrders);
+       document.getElementById("demo").innerHTML = `${price} Ksh`;
+       alert(`amount due for your order is ksh. ${price}`);
+       anotherOrderPrompt = prompt('would you like to make another order? Yes/No');
+       if(anotherOrderPrompt ==='no' || anotherOrderPrompt === 'No' ) {
+          alert('welcome');
        }
-     document.getElementById("demo").innerHTML = tprice + " KSh";
+     
+       }
+       calculateTotal.addEventListener('click', (event) => total(event));
+     
       
-    }
+    
